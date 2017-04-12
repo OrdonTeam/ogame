0a537254489a5d77ef083cec8bc7b78d6bc59051
Karol Kowalski <ordon1313@gmail.com>
2017-04-12 20:52:33 +0200

    Add get fleet movement controller

--- core/src/main/java/io/github/ordonteam/ogame/Application.kt
+++ core/src/main/java/io/github/ordonteam/ogame/Application.kt
@@ -1,25 +1,15 @@
-//package io.github.ordonteam.ogame
-//
-//import org.openqa.selenium.WebDriver
-//import org.openqa.selenium.phantomjs.PhantomJSDriver
-//import org.springframework.boot.SpringApplication
-//import org.springframework.boot.autoconfigure.SpringBootApplication
-//import org.springframework.context.annotation.Bean
-//
-//@SpringBootApplication
-//open class Application {
-//
-//    @Bean
-//    fun webDriver(): WebDriver {
-//        return webDriver
-//    }
-//
-//    companion object {
-//        val webDriver = PhantomJSDriver()
-//
-//        @JvmStatic
-//        fun main(args: Array<String>) {
-//            SpringApplication.run(Application::class.java, *args)
-//        }
-//    }
-//}
+package io.github.ordonteam.ogame
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class Application {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
--- core/src/main/java/io/github/ordonteam/ogame/getFleetMovment.kt
+++ core/src/main/java/io/github/ordonteam/ogame/getFleetMovment.kt
@@ -4,11 +4,11 @@ import org.openqa.selenium.WebDriver
 
 fun getFleetMovement(webDriver: WebDriver): List<FleetMovement> {
     webDriver.get("https://s146-pl.ogame.gameforge.com/game/index.php?page=overview")
-    if (webDriver.hasElementWithId("eventboxBlank")) {
-        return emptyList()
-    } else {
+    if (webDriver.hasElementWithId("js_eventDetailsClosed")) {
         webDriver.findElementById("js_eventDetailsClosed").click()
         return webDriver.findElementsByClassName("eventFleet").map { FleetMovement(it.hasElementWithClassName("friendly")) }
+    } else {
+        return emptyList()
     }
 }
 

dc380979cf0389a32c809c4709ba25c156288f3a
Karol Kowalski <ordon1313@gmail.com>
2017-05-21 00:33:39 +0200

    Refactor spy

--- core/src/main/java/io/github/ordonteam/ogame/script/functions/spy.kt
+++ core/src/main/java/io/github/ordonteam/ogame/script/functions/spy.kt
@@ -17,40 +17,15 @@ private val FLEET_TABLE = "body > center > center > table > tbody > tr > td:nth-
 
 fun RemoteWebDriver.generateNewSpyReport(sin: String, galaxy: Int, system: Int, planet: Int): SpyReport {
     spy(sin, Position(galaxy, system, planet))
-    val report = readReport(sin, galaxy, system, planet)
-    return report
+    waitForReport(sin, galaxy, system, planet)
+    val result = readReport()
+    deleteReport()
+    return result
 }
 
 private fun RemoteWebDriver.spy(sin: String, position: Position) {
     sendFleet(sin, position, Fleet(mapOf(Ship.EXPANSION_PROBE to 1000L)), Mission.SPY)
 }
-
-private fun RemoteWebDriver.readReport(sin: String, galaxy: Int, system: Int, planet: Int): SpyReport {
-    return doReadReport(sin, galaxy, system, planet)
-}
-
-private fun RemoteWebDriver.doReadReport(sin: String, galaxy: Int, system: Int, planet: Int): SpyReport {
-    waitForReport(sin, galaxy, system, planet)
-    val result = SpyReport(
-            metal = findElementByCssSelector(METAL_IN_REPORT).toLongWithoutDots(),
-            crystal = findElementByCssSelector(CRYSTAL_IN_REPORT).toLongWithoutDots(),
-            deuterium = findElementByCssSelector(DEUTERIUM_IN_REPORT).toLongWithoutDots(),
-            fleet = readFleet())
-    findElementByCssSelector("body > center > center > table > tbody > tr > td:nth-child(2) > table:nth-child(2) > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(4) > th:nth-child(2) > input[type=\"checkbox\"]").click()
-    findElementByCssSelector("body > center > center > table > tbody > tr > td:nth-child(2) > table:nth-child(2) > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(1) > th > input[type=\"submit\"]").click()
-    return result
-}
-
-private fun RemoteWebDriver.readFleet(): Fleet {
-    val hasShips = findElementByCssSelector(FLEET_TABLE).findElements(By.tagName("tr")).size == 1
-    if (hasShips) {
-        return Fleet(mapOf(Ship.ULTRA_TRANSPORTER to 1L))
-    } else {
-        return Fleet(emptyMap())
-    }
-}
-
-private fun WebElement.toLongWithoutDots() = text.replace(".", "").toLong()
 
 private fun RemoteWebDriver.waitForReport(sin: String, galaxy: Int, system: Int, planet: Int) {
     (0..10).forEach {
@@ -72,4 +47,29 @@ private fun RemoteWebDriver.isReportAvailable(sin: String, galaxy: Int, system: 
         return report.text.contains("[$galaxy:$system:$planet]")
     }
 }
+
+private fun RemoteWebDriver.readReport(): SpyReport {
+    val result = SpyReport(
+            metal = findElementByCssSelector(METAL_IN_REPORT).toLongWithoutDots(),
+            crystal = findElementByCssSelector(CRYSTAL_IN_REPORT).toLongWithoutDots(),
+            deuterium = findElementByCssSelector(DEUTERIUM_IN_REPORT).toLongWithoutDots(),
+            fleet = readFleet())
+    return result
+}
+
+private fun RemoteWebDriver.readFleet(): Fleet {
+    val hasShips = findElementByCssSelector(FLEET_TABLE).findElements(By.tagName("tr")).size == 1
+    if (hasShips) {
+        return Fleet(mapOf(Ship.ULTRA_TRANSPORTER to 1L))
+    } else {
+        return Fleet(emptyMap())
+    }
+}
+
+private fun RemoteWebDriver.deleteReport() {
+    findElementByCssSelector("body > center > center > table > tbody > tr > td:nth-child(2) > table:nth-child(2) > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(4) > th:nth-child(2) > input[type=\"checkbox\"]").click()
+    findElementByCssSelector("body > center > center > table > tbody > tr > td:nth-child(2) > table:nth-child(2) > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(1) > th > input[type=\"submit\"]").click()
+}
+
+private fun WebElement.toLongWithoutDots() = text.replace(".", "").toLong()
 

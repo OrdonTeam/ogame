19978757dadc7202287b3ca4152d1d6b100ea7c2
Karol Kowalski <ordon1313@gmail.com>
2017-05-20 23:48:07 +0200

    Add planet type

--- core/src/main/java/io/github/ordonteam/ogame/script/functions/sendFleet.kt
+++ core/src/main/java/io/github/ordonteam/ogame/script/functions/sendFleet.kt
@@ -5,7 +5,12 @@ import io.github.ordonteam.ogame.script.model.Position
 import org.openqa.selenium.remote.RemoteWebDriver
 
 fun RemoteWebDriver.sendFleet(sin: String, target: Position, fleet: Fleet) {
-    get("http://uni9.ogam.net.pl/index.php?page=fleet&from=rs&mode=raport&galaxy=${target.galaxy}&system=${target.system}&planet=${target.planet}&planettype=1&target_mission=1")
+    get("http://uni9.ogam.net.pl/index.php?page=fleet&from=rs&mode=raport" +
+            "&galaxy=${target.galaxy}" +
+            "&system=${target.system}" +
+            "&planet=${target.planet}" +
+            "&planettype=${target.typePlanet.type}" +
+            "&target_mission=1")
     fleet.ships.forEach { ship, count ->
         findElementById(ship.id).sendKeys(count.toString())
     }
--- core/src/main/java/io/github/ordonteam/ogame/script/model/Position.kt
+++ core/src/main/java/io/github/ordonteam/ogame/script/model/Position.kt
@@ -1,3 +1,7 @@
 package io.github.ordonteam.ogame.script.model
 
-data class Position(val galaxy: Int, val system: Int, val planet: Int)
+data class Position(val galaxy: Int, val system: Int, val planet: Int, val typePlanet: PlanetType = PlanetType.Planet)
+
+enum class PlanetType(val type: String) {
+    Planet("1")
+}

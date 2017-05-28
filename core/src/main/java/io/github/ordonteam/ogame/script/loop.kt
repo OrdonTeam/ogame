fc3e7036305f5d292c49430e968ad1c7ce6793b2
Karol Kowalski <ordon1313@gmail.com>
2017-05-28 17:39:48 +0200

    Limit logger

--- core/src/main/java/io/github/ordonteam/ogame/script/loop.kt
+++ core/src/main/java/io/github/ordonteam/ogame/script/loop.kt
@@ -4,6 +4,7 @@ import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
 import java.net.URL
 import java.util.logging.Level
+import java.util.logging.Logger
 
 fun loopForEver(block: RemoteWebDriver.() -> Unit) {
     while (true) {
@@ -19,7 +20,7 @@ fun loopForEver(block: RemoteWebDriver.() -> Unit) {
 
 fun loopOnce(block: RemoteWebDriver.() -> Unit) {
     val driver = RemoteWebDriver(URL("http://localhost:9515"), DesiredCapabilities.chrome()).apply {
-        setLogLevel(Level.WARNING)
+        Logger.getLogger(RemoteWebDriver::class.java.name).level = Level.OFF
     }
     try {
         driver.block()

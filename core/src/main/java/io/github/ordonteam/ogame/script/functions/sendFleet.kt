82d66d4d2037b9e2d99cc26fc78678c3537d2bce
Karol Kowalski <ordon1313@gmail.com>
2017-05-28 23:21:58 +0200

    Wait for fleet return

--- core/src/main/java/io/github/ordonteam/ogame/script/functions/sendFleet.kt
+++ core/src/main/java/io/github/ordonteam/ogame/script/functions/sendFleet.kt
@@ -4,6 +4,8 @@ import io.github.ordonteam.ogame.script.model.Fleet
 import io.github.ordonteam.ogame.script.model.Position
 import org.openqa.selenium.remote.RemoteWebDriver
 
+private val TIME_TO_FIRST_RETURN = "#time_0 > font"
+
 fun RemoteWebDriver.sendFleet(sin: String, target: Position, fleet: Fleet, mission: Mission) {
     get("http://uni9.ogam.net.pl/index.php?page=fleet&from=rs&mode=raport" +
             "&galaxy=${target.galaxy}" +
@@ -11,12 +13,26 @@ fun RemoteWebDriver.sendFleet(sin: String, target: Position, fleet: Fleet, missi
             "&planet=${target.planet}" +
             "&planettype=${target.typePlanet.type}" +
             "&target_mission=${mission.type}")
-    fleet.ships.forEach { ship, count ->
-        findElementById(ship.id).sendKeys(count.toString())
+    if (findElementsByClassName("planet").isEmpty()) {
+        waitForReturn()
+        sendFleet(sin, target, fleet, mission)
+    } else {
+        fleet.ships.forEach { ship, count ->
+            findElementById(ship.id).sendKeys(count.toString())
+        }
+        findElementByClassName("planet").click()
+        findElementByXPath("//input[@type='submit']").click()
+        findElementByXPath("//input[@type='submit']").click()
     }
-    findElementByClassName("planet").click()
-    findElementByXPath("//input[@type='submit']").click()
-    findElementByXPath("//input[@type='submit']").click()
+}
+
+private fun RemoteWebDriver.waitForReturn() {
+    val timeToReturn = findElementByCssSelector(TIME_TO_FIRST_RETURN).text
+    val minutes = timeToReturn.split(" ")[1].dropLast(1).toLong()
+    val seconds = timeToReturn.split(" ")[2].dropLast(1).toLong()
+    val millis = ((minutes * 60) + seconds + 1) * 1000
+    System.err.println("ALL SLOTS IN USE -- WAITING ${millis / 1000L}")
+    Thread.sleep(millis)
 }
 
 enum class Mission(val type: String) {
